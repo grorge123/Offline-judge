808b22dcbf0836c843f95fb5d7dab6550aac3ad8
grorge <g97116@yahoo.com>
2018-09-15 20:23:08 +0800

    add judge

---
+++ judge/inp/p1inp.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b;
+    while(cin >> a>> b){
+//        for(int i=0;i<100000;i++){
+//            for(int q=0;q<1;q++){
+//                for(int d=0;d<1;d++){
+//                    int e=i+q+d;
+//                    cout << e<<endl;
+//                }
+//            }
+//        }
+        cout << a+b<<endl;
+        cout << endl;
+    }
+	return 0;
+}
